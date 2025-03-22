4a44cc5d61a02ec077c89d44f6132a653c8192aa
andi-frame <andifarhan1094@gmail.com>
2025-03-23 04:05:51 +0700

    fix: problem with tailwind v4 padding and margin

--- frontend/postcss.config.mjs
+++ frontend/postcss.config.mjs
@@ -1,4 +1,11 @@
 const config = {
+  content: [
+    "./src/app/**/*.{js,ts,jsx,tsx}", // For Next.js (App Router)
+    "./src/components/**/*.{js,ts,jsx,tsx}", // For Components
+  ],
+  theme: {
+    extend: {},
+  },
   plugins: ["@tailwindcss/postcss"],
 };
 
