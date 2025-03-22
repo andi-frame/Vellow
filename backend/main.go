ba48fa04da061d3598c37247dac6e669bca69182
andi-frame <andifarhan1094@gmail.com>
2025-03-23 01:22:45 +0700

    feat: add main.go integrated with auth

--- backend/main.go
+++ backend/main.go
@@ -1,15 +1,32 @@
 package main
 
 import (
+	// "github.com/andi-frame/Vellow/backend/database"
+	"log"
+	"os"
+
 	"github.com/andi-frame/Vellow/backend/database"
 	"github.com/andi-frame/Vellow/backend/routes"
+	Auth "github.com/andi-frame/Vellow/backend/services/auth"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	Auth.NewAuth()
 	database.InitDB()
 
-	r := gin.Default()
-	routes.Setup(r)
-	r.Run(":8080")
+	gin.SetMode(gin.DebugMode)
+
+	r := routes.SetupRouter()
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	// Start server
+	log.Printf("Server running on port %s", port)
+	if err := r.Run(":" + port); err != nil {
+		log.Fatalf("Failed to start server: %v", err)
+	}
 }
