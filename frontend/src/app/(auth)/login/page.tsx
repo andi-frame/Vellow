5bbe252c26e0350af4ebdd8c15d818d26f735250
andi-frame <andifarhan1094@gmail.com>
2025-03-23 16:12:35 +0700

    feat: done auth login with google fe

--- frontend/src/app/(auth)/login/page.tsx
+++ frontend/src/app/(auth)/login/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import { loginWithProvider, logout, fetchUser } from "@/lib/api";
+import { loginWithProvider, fetchUser } from "@/lib/api";
 import { useQuery, useMutation } from "@tanstack/react-query";
 import Image from "next/image";
 
@@ -43,7 +43,7 @@ const LoginPage = () => {
   // });
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen text-white gap-12 w-sm">
+    <div className="flex flex-col items-center justify-center h-screen text-white gap-10 w-sm">
       <div className="flex flex-col items-center justify-center w-5/6">
         <Image width={150} height={150} src={"/vellow-text.svg"} alt="vellow-text"></Image>
         <Image className="py-3" width={200} height={200} src={"/vellow-logo.svg"} alt="vellow-logo"></Image>
@@ -57,7 +57,7 @@ const LoginPage = () => {
       <div className="w-5/6 bg-[#A467EE] ring-1 ring-[#C69EF6] rounded-3xl flex flex-col justify-center items-center text-center p-5">
         <p className="text-2xl font-bold">Welcome To Vellow</p>
         <p className="text-sm font-light">Get fit together with your friends and family</p>
-        <div className="flex flex-col py-5 w-2/3">
+        <div className="flex flex-col py-12 w-2/3">
           <button
             className="rounded-2xl bg-[#1E1E1E]/20 px-4 py-3"
             onClick={() => loginMutation.mutate("google")}
