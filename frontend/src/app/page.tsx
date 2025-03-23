0a537afacebcae264ef22e7df9e9257ca64baa2c
andi-frame <andifarhan1094@gmail.com>
2025-03-23 23:40:43 +0700

    feat: add redirect

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,11 @@
+import { redirect } from "next/navigation";
+
 export default function Home() {
-  return (
-    <div className="flex justify-center items-center w-full h-full">
-      <div className="bg-white p-5">Test</div>
-    </div>
-  );
+  redirect("/login");
+  
+  // return (
+  //   <div className="flex justify-center items-center w-full h-full">
+  //     <div className="bg-white p-5">Test</div>
+  //   </div>
+  // );
 }
